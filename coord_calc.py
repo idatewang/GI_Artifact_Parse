87909776ab39191ec5f8476a50dfea87c2d392b2
idatewang <idatewang@tamu.edu>
2021-07-26 04:27:52 -0500

    Unlock worked, files renamed, new picture templates added.

---
+++ coord_calc.py
@@ -0,0 +1,34 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
+from ctypes import windll
+import mouse
+
+method = cv2.TM_SQDIFF_NORMED
+user32 = windll.user32
+user32.SetProcessDPIAware()
+
+screen = np.array(ImageGrab.grab(bbox=(1560, 450, 1620, 500)))
+screen_array = cv2.cvtColor(screen, cv2.COLOR_RGB2BGR)
+
+small_image = cv2.imread('lock.JPG')
+
+result = cv2.matchTemplate(small_image, screen_array, method)
+mn, _, mnLoc, _ = cv2.minMaxLoc(result)
+
+# Draw the rectangle:
+# Extract the coordinates of our best match
+MPx, MPy = mnLoc
+print((1 - mn) * 100, MPx + 350, MPy + 190)
+mouse.position = (MPx + 350, MPy + 190)
+# Step 2: Get the size of the template. This is the same size as the match.
+trows, tcols = small_image.shape[:2]
+#
+# # Step 3: Draw the rectangle on large_image
+cv2.rectangle(screen_array, (MPx, MPy), (MPx + tcols, MPy + trows), (0, 0, 255), 2)
+
+# # Display the original image with the rectangle around the match.
+cv2.imshow('output', screen_array)
+#
+# # The image is only displayed if we call this
+cv2.waitKey(0)
--- main.py
+++ main.py
@@ -1,25 +1,62 @@
+import sys
 from pynput.mouse import *
 import numpy as np
 from PIL import ImageGrab
 import cv2
-from urllib.request import urlopen
 from ctypes import windll
+import pyautogui
+import time
+import math
+import keyboard
 
 user32 = windll.user32
 user32.SetProcessDPIAware()
 mouse = Controller()
 
-if __name__ == "__main__":
-    count = 0
-    x = 0
-    y = 0
-    x_coord = 400
-    y_coord = 270
-    while count <= 979:
-        mouse.position = (x_coord, y_coord)
-        mouse.click(Button.left)
 
-        if x_coord != 1120:
-            x_coord += 120
-        else:
-            x_coord = 0
+def unlock():
+    screen = np.array(ImageGrab.grab(bbox=(1560, 450, 1620, 500)))
+    screen_array = cv2.cvtColor(screen, cv2.COLOR_RGB2BGR)
+    small_image = cv2.imread('lock.JPG')
+    result = cv2.matchTemplate(small_image, screen_array, method)
+    mn, _, _, _ = cv2.minMaxLoc(result)
+    if 1 - mn > 0.9:
+        pyautogui.moveTo(1580, 465, 0)
+        time.sleep(0.5)
+        pyautogui.click()
+        mouse.position = (x, y)
+
+
+if __name__ == "__main__":
+    method = cv2.TM_SQDIFF_NORMED
+    x = 410
+    y = 270
+    s = 49
+    mouse.position = (x, y)
+    mouse.click(Button.left)
+    for i in range(math.floor(909 / 35)):
+        screen = np.array(ImageGrab.grab(bbox=(350, 190, 472, 370)))
+        screen_array = cv2.cvtColor(screen, cv2.COLOR_RGB2BGR)
+        small_image = cv2.imread('spot.JPG')
+        result = cv2.matchTemplate(small_image, screen_array, method)
+        mn, _, mnLoc, _ = cv2.minMaxLoc(result)
+        MPx, MPy = mnLoc
+        x = MPx + 350
+        y = MPy + 190
+        if keyboard.is_pressed("q"):
+            sys.exit()
+        for u in range(35):
+            if keyboard.is_pressed("q"):
+                sys.exit()
+            if u != 0 and (u + 0) % 7 == 0:
+                y += 140
+                x = 410
+            mouse.position = (x, y)
+            mouse.click(Button.left)
+            time.sleep(0.5)
+            unlock()
+            x += 115
+        for o in range(s):
+            pyautogui.scroll(-1)
+            if keyboard.is_pressed("q"):
+                sys.exit()
